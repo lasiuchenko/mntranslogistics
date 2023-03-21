3308ee035affbeed22a4c5f3e530f6c503fb247c
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2023-03-21 06:40:19 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+function styles() {
+	return src('app/scss/style.scss')
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+		.pipe(concat('style.min.css'))
+		.pipe(scss({ outputStyle: 'compressed' }))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function scripts() {
+	return src([
+		// 'node_modeles/swiper-bundle.js',
+		'app/js/main.js'
+		// 'app/js/*.js',
+		// '!app/js/main.min.js'
+	])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function images() {
+	return src('app/images/**/*.*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(dest('dist/images'))
+}
+
+function watching() {
+	watch(['app/scss/**/*.scss'], styles)
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+	watch(['app/**/*.html']).on('change', browserSync.reload)
+}
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		},
+		notify: false
+	});
+};
+
+function building() {
+	return src([
+		'app/css/style.min.css',
+		'app/js/main.min.js',
+		'app/**/*.html'
+	], { base: 'app' })
+		.pipe(dest('dist'))
+}
+
+function cleanDist() {
+	return src('dist')
+		.pipe(clean())
+}
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.images = images;
+
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, browsersync, watching);
